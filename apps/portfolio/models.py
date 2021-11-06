9aaa96b4f0fffd11fc2369a7a0d76c113b77da88
Maury Areas <mareasperez7@gmail.com>
2021-11-05 20:17:03 -0600

    feat:  model projects

--- apps/portfolio/models.py
+++ apps/portfolio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.fields import CharField, TextField, URLField
+from django.db.models.fields.files import ImageField
 
-# Create your models here.
+
+class Projects(models.Model):
+    title = CharField(max_length=100)
+    description = TextField()
+    image = ImageField(upload_to='portfolio/images/')
+    url = URLField(blank=True)
+
+    def __str__(self):
+        return self.title
